7ebfe3b3e9b4e007f0f106377297373068358440
Mason Wong <masonwong19@gmail.com>
2022-10-08 11:53:41 -0700

    Refactoring to meet the specs of asst3 better

--- kern/synchprobs/airballoon.c
+++ kern/synchprobs/airballoon.c
@@ -21,12 +21,16 @@ struct rope {
 	struct lock* lk;
 };
 
+struct hook {
+	struct rope *rope;
+};
+
 struct stake {
-	volatile int rope_num;
+	volatile struct rope *rope;
 	struct lock* lk;
 };
 
-static struct rope *ropes[NROPES];
+static struct hook *hooks[NROPES];
 static struct stake *stakes[NROPES];
 
 /* Synchronization primitives */
@@ -48,17 +52,21 @@ static volatile int num_threads = N_LORD_FLOWERKILLER + 3;
  * 		- a rope will have a unique rope number (the rope number is also equivalent to a hook number because hooks and ropes dont get swapped around)
  * 		- a rope can be severed or not severed
  * 		- a rope has a lock so that only one actor can handle the rope at a time
+ *	`struct hook`
+ *		- this data structure defines a hook
+ *		- a hook can point to a `struct rope` that is connected to it
+ * 		- does not require a lock because only dandelion can access these
  * 	`struct stake`
  * 		- this data structure defines a stake
- * 		- a stake will be able to identify a rope with a rope num which is declared as volatile because it can be changed
+ * 		- a stake will be able to point to a `struct rope` that is connected to it
  * 		- a stake has a lock so that only one actor can handle the stake at a time
- * 	`struct rope *ropes[]`
- * 		- this is an array used to index our ropes
- * 		- ropes[n] will mean that you are accessing rope n (rope n also corresponds to hook n because hooks do not change); 0 <= n < NROPES
- * 		- there are NROPES ropes in the array
+ *	`struct hook *hooks[]`
+ * 		- this is an array used to index our hooks
+ * 		- hooks[n] corresponds to hook n; 0 <= n < NROPES
+ * 		- there is one hook per rope in the array
  * 	`struct stake *stakes[]`
  * 		- this is an array used to to index array our stakes
- * 		- stake[n] corresponds to stake n; 0 <= n < NROPES
+ * 		- stakes[n] corresponds to stake n; 0 <= n < NROPES
  * 		- there is one stake per rope in the array
  * 
  * Synchronization Primitives:
@@ -73,29 +81,32 @@ static volatile int num_threads = N_LORD_FLOWERKILLER + 3;
  * Functions Implemented:
  * 	`dandelion`
  * 		- the dandelion function will run a while loop until the `ropes_left` global variable gets decremented to 0
- * 		- in the loop `dandelion` will pick a random hook number and with this hook number it will index the `ropes` array to get a rope
- * 		- the `dandelion` thread will try to acquire the lock before going any futher
+ * 		- in the loop `dandelion` will pick a random hook number and with this hook number it will index the `hooks` array to get a hook
+ * 		- the `dandelion` thread will try to acquire the lock hook->rope->lk before going any futher
  * 		- once the lock is acquired we check if the rope is severed, if it is not we set the rope to be severed and print out that dandelion severed that rope
  * 		- the lock is then released and the thread yields
  * 		- when the loop exits, we print that dandelion has completed and we acquire the num_threads_lk lock and then decrement num_threads then release num_threads_lk
+ * 		- if the rope was already severed then we release the lock and we continue to the next iteration of the loop without yielding
  * 	`marigold`
  * 		- the marigold function will run a while loop until the `ropes_left` global variable gets decremented to 0
  * 		- in the loop marigold will pick a random stake number and with the stake number it will index the `stakes` array to get a stake
- * 		- the marigold thread will try to acquire the lock that corresponds to the stake before going any further
- * 		- once the lock is acquired for the stake, we use the `rope_num` associated with the stake to index the `ropes` array
- * 		- we then acquire the lock that corresponds to the rope we have indexed in the `ropes` array
- * 		- once we have acquire the lock we can check if the rope is severed, if it is not severed we can set it as severed then print that marigold has severed rope rope_num from hook hook_num
+ * 		- the marigold thread will try to acquire the lock , stake->lk, before going any further
+ * 		- once that lock is acquired for the stake, we then acquire the lock stake->rope->lk
+ * 		- once we have acquire that lock we can check if the rope is severed, if it is not severed we can set it as severed then print that marigold has severed rope->rope_num from the hook number
  * 		- the locks that we have acquired are then released and the thread yields
+ * 		- if the rope was already severed we will release the locks and continue to the next iteration of the loop without yielding
  * 		- the loop will terminate when `ropes_left` gets decremented down to 0
  * 		- once the loop terminates we print that the marigold thread is done then decremenet the num_threads counter once we have acquired the lock for it
  * 	`flowerkiller`
  * 		- the flowerkiller thread will loop until ropes_left is less than 1. at this point flowerkiller can no longer swap ropes so it is done.
  * 		- starts by generating two random stake numbers, stake_num1 and stake_num2
  * 		- the flowerkiller thread will then acquire the flowerkiller_lk lock to prepare for acquiring stake and rope locks (we don't want multiple flowerkiller threads doing this at once because it will lead to race condition)
- * 		- flowerkiller will first acquire the lock corresponding to stakes[stake_num1] and then acquire the rope for the rope_num corresponding to the stake
+ * 		- flowerkiller will first acquire the lock stakes[stake_num1]->lk and then acquire the lock for stakes[stake_num1]->rope->lk
  * 		- the same step as above will follow for stake_num2
- * 		- once the locks are acquired we swap the rope_num corresponding to stake1 with the rope_num corresponding to stake2. We then report the swaps in print statements
+ * 		- once the locks are acquired we swap the rope assocaited to stake1 with the rope associated to stake2. We then report the swaps in print statements
  * 		- flowerkiller then releases the locks and the thread will yield
+ * 		- if flowerkiller happened to select stake_nums s.t. stake_num1 == stake_num2 we will continue to next iteration without yielding
+ * 		- if flowerkiller happened to select stakes that have ropes that are already severed then we will release our locks and we will continue to next iteration of loop without yielding
  * 		- once the loop ends the thread will print that it is has completed and it will decrement the num_threads counter once it has acquired the lock to do so	
  * `balloon`
  * 		- the balloon thread will sit in a while loop until the thread_count gets decremented to 1 which indicates that its the only thread remaining (other than the main thread)
@@ -118,26 +129,27 @@ dandelion(void *p, unsigned long arg)
 	// Dandelion will unhook the ropes on the balloon.
 	(void)p;
 	(void)arg;
-
+	struct rope *rope;
 	kprintf("Dandelion thread starting\n");
 
 	/* Implement this function */
 	while (ropes_left > 0) {
 		int hook = random() % NROPES; // the hook num and rope num should always be the same becuase hooks don't swap
-		lock_acquire(ropes[hook]->lk);
-		if (!ropes[hook]->severed) {
+		rope = hooks[hook]->rope;
+		lock_acquire(rope->lk);
+		if (!rope->severed){
 			ropes_left--;
-			ropes[hook]->severed = true;
+			rope->severed = true;
 
 			lock_acquire(kprintf_lk);
 			kprintf("Dandelion severed rope %d\n", hook);
 			lock_release(kprintf_lk);
 		}
 		else {
-			lock_release(ropes[hook]->lk);
+			lock_release(rope->lk);
 			continue; // don't yield if rope was severed.
 		}
-		lock_release(ropes[hook]->lk);
+		lock_release(rope->lk);
 		thread_yield();
 	}
 
@@ -155,7 +167,7 @@ marigold(void *p, unsigned long arg)
 	// Marigold will sever the stakes on the ground.
 	(void)p;
 	(void)arg;
-	struct rope* rope;
+	volatile struct rope* rope;
 
 	kprintf("Marigold thread starting\n");
 
@@ -163,7 +175,7 @@ marigold(void *p, unsigned long arg)
 	while (ropes_left > 0) {
 		int stake = random() % NROPES;
 		lock_acquire(stakes[stake]->lk);
-		rope = ropes[stakes[stake]->rope_num];
+		rope = stakes[stake]->rope;
 		lock_acquire(rope->lk);
 		if (!rope->severed) {
 			ropes_left--;
@@ -196,7 +208,7 @@ flowerkiller(void *p, unsigned long arg)
 {
 	(void)p;
 	(void)arg;
-	struct rope *rope1, *rope2;
+	volatile struct rope *rope1, *rope2;
 	int stake_num1, stake_num2;	
 
 	kprintf("Lord FlowerKiller thread starting\n");
@@ -212,10 +224,10 @@ flowerkiller(void *p, unsigned long arg)
 
 		lock_acquire(flower_killer_lk);
 		lock_acquire(stakes[stake_num1]->lk);
-		rope1 = ropes[stakes[stake_num1]->rope_num];
+		rope1 = stakes[stake_num1]->rope;
 		lock_acquire(rope1->lk);
 		lock_acquire(stakes[stake_num2]->lk);
-		rope2 = ropes[stakes[stake_num2]->rope_num];
+		rope2 = stakes[stake_num2]->rope;
 		lock_acquire(rope2->lk);
 		lock_release(flower_killer_lk);
 
@@ -229,8 +241,8 @@ flowerkiller(void *p, unsigned long arg)
 		}
 
 		// swap the ropes for the two stakes
-		stakes[stake_num1]->rope_num = rope2->rope_num;
-		stakes[stake_num2]->rope_num = rope1->rope_num;
+		stakes[stake_num1]->rope = rope2;
+		stakes[stake_num2]->rope = rope1;
 
 		lock_acquire(kprintf_lk);
 		kprintf("Lord FlowerKiller switched rope %d from stake %d to stake %d\n", rope1->rope_num, stake_num1, stake_num2);
@@ -262,7 +274,7 @@ balloon(void *p, unsigned long arg)
 	kprintf("Balloon thread starting\n");
 
 	/* Implement this function */
-	while (num_threads > 1) {/* maybe we make this sleep on a cv */}
+	while (num_threads > 1) {}
 
 	kprintf("Balloon freed and Prince Dandelion escapes!\n");
 	kprintf("Balloon thread done\n");
@@ -272,29 +284,32 @@ balloon(void *p, unsigned long arg)
 	lock_release(num_threads_lk);
 }
 
-
 // Change this function as necessary
 int
 airballoon(int nargs, char **args)
 {
 	int err = 0, i;
+	struct rope *rope;
 	num_threads_lk = lock_create("Num Threads Lock");
 	flower_killer_lk = lock_create("Flower killer lock");
 	kprintf_lk = lock_create("kprintf lock");
 	for (i = 0; i < NROPES; i++) {
-		ropes[i] = (struct rope *)kmalloc(sizeof(struct rope));
-		ropes[i]->rope_num = i;
-		ropes[i]->severed = false;
-		ropes[i]->lk = lock_create("A rope lock");
+		rope = (struct rope *)kmalloc(sizeof(struct rope));
+		rope->rope_num = i;
+		rope->severed = false;
+		rope->lk = lock_create("A rope lock");
 
-		stakes[i] = (struct stake *) kmalloc(sizeof(struct stake));
-		stakes[i]->rope_num = i;
+		hooks[i] = (struct hook *)kmalloc(sizeof(struct hook));
+		hooks[i]->rope = rope;
+
+		stakes[i] = (struct stake *)kmalloc(sizeof(struct stake));
+		stakes[i]->rope = rope;
 		stakes[i]->lk = lock_create("A stake lock");
 	}
 
 	(void)nargs;
 	(void)args;
-	(void)ropes_left;
+	//(void)ropes_left;
 
 	err = thread_fork("Marigold Thread",
 			  NULL, marigold, NULL, 0);
@@ -330,9 +345,13 @@ airballoon(int nargs, char **args)
 	lock_destroy(flower_killer_lk);
 	lock_destroy(kprintf_lk);
 	for (i = 0; i < NROPES; i++) {
-		lock_destroy(ropes[i]->lk);
-		kfree(ropes[i]);
-		ropes[i] = NULL;
+		rope = hooks[i]->rope;
+		lock_destroy(rope->lk);
+		kfree(rope);
+		rope = NULL;
+		
+		kfree(hooks[i]);
+		hooks[i] = NULL;
 
 		lock_destroy(stakes[i]->lk);
 		kfree(stakes[i]);
